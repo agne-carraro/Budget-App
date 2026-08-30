a8959f2d23b2a010e7a108bed7e0d600f6a704ba
agnese <agnese.carraro@studenti.unipd.it>
2026-08-30 16:12:50 +0200

    Initial Commit

---
+++ Budget AppTests/Budget_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Budget_AppTests.swift
+//  Budget AppTests
+//
+//  Created by Agnese Carraro on 2026-08-30.
+//
+
+import Testing
+@testable import Budget_App
+
+struct Budget_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
